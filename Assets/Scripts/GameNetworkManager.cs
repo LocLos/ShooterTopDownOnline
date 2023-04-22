6c5aaaca1ac02d204ca6acc4f5f6d66375e1c44c
LocLos <43320466+LocLos@users.noreply.github.com>
2023-04-22 17:28:38 +0300

    Add  sprites, scripts- shoot, moving, CameraController, bullet,health

---
+++ Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace ShooterTopDown
+{
+    public class GameNetworkManager : NetworkManager
+    {
+        [SerializeField] private Transform leftPos, rightPos;
+
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+            GameObject player = numPlayers == 1
+                ? Instantiate(playerPrefab, leftPos.position, leftPos.rotation)
+                : Instantiate(playerPrefab, rightPos.position, rightPos.rotation);
+
+
+            player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
+            NetworkServer.AddPlayerForConnection(conn, player);
+        }
+    }
+}
